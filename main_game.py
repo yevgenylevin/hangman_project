8196a840b667ac8fe5d657a05d4244c180d5f868
Yevgeny Levin <leyevge@gmail.com>
2025-11-09 21:34:12 +0200

    adddaed input function

--- main_game.py
+++ main_game.py
@@ -1,1 +1,10 @@
 print("Welcome to hangman game!")
+
+def get_letter():
+    letter = input("Enter a letter: ")
+
+    # Check if input is one character and alphabetical
+    if len(letter) == 1 and letter.isalpha():
+        return letter
+    else:
+        return False
